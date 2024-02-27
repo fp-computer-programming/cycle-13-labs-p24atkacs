9daa0371139e2ac3ee3a9f585ba563076b8f7a69
Andrew Tkacs <p24atkacs@fairfieldprep.org>
2024-02-27 12:26:29 -0500

    lab 13-4

---
+++ lab_13-4.py
@@ -0,0 +1,21 @@
+
+#Author: Andrew Tkacs
+
+#Computer Programming
+
+#Lab 13-4
+
+#  I Created a list called day_date that contains the number day for the next 5 days.
+day_date = [2, 3, 4, 5, 6]
+
+# I used the tuple and assigned the 5 letters from the list to a variable that represents each day using a single statement.
+mon, tue, wed, thu, fri = tuple(day_date)
+
+# Prints each of them to get what number I set them equal to.  
+print("mon:", mon)
+print("tue:", tue)
+print("wed:", wed)
+print("thu:", thu)
+print("fri:", fri)
+
+#works.
