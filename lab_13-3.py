6e1aa97c9dd93a7bb2cb16d62a74f587e7e7f166
Andrew Tkacs <p24atkacs@fairfieldprep.org>
2024-02-27 12:19:01 -0500

    Lab 13-3

---
+++ lab_13-3.py
@@ -0,0 +1,23 @@
+
+
+#Author: Andrew Tkacs
+
+#Computer Programming
+
+#Lab 13-3
+
+# This variable (weekdays) I set equal to a tuple that contains the days Monday - Friday as strings
+
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# Create a variable, weekends, and set it equal to a tuple that contains Saturday and Sunday as strings
+
+weekends = ("Saturday", "Sunday")
+
+# Create a variable called days_of_the_week and set it equal to the concatenation of the two previous variables.
+
+days_of_the_week = weekdays + weekends
+
+
+#prints all of the days of the week in order.
+print(days_of_the_week)
