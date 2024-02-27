57f063ee9e5218c8d3e33c612f387f962eb86ffd
Andrew Tkacs <p24atkacs@fairfieldprep.org>
2024-02-27 12:08:55 -0500

    lab_13-1

---
+++ lab_13-1.py
@@ -0,0 +1,24 @@
+
+#Author: Andrew Tkacs
+
+# Computer Programming
+
+# Lab 13-1
+
+row = {}  # created dictionary, set it equal to row
+
+# Add key value pairs to the dictionary for each person in the row
+row["computer1"] = "Andrew"
+row["computer2"] = "Robert"
+row["computer3"] = "Jay"
+row["computer4"] = "Mrs. K"
+
+print(row)
+
+# Update the dictionary to reflect someone else sitting at your computer
+row["computer2"] = "Madison"
+
+# Print the updated dictionary
+print(row)
+
+#works
