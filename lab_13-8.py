db53370bb255884b215dd62df5c6edb0a12048b8
Andrew Tkacs <p24atkacs@fairfieldprep.org>
2024-02-29 09:26:05 -0500

    lab 13-8

---
+++ lab_13-8.py
@@ -0,0 +1,15 @@
+
+#author: Andrew Tkacs
+
+#Computer Programming
+
+#lab 13-8
+
+# Open the file for writing
+with open('my_file.txt', 'w') as file:
+    # Write the lines to the file
+    file.write("Initials: A.T.\n")
+    file.write("Date: 2/29/24\n")
+    file.write("Hello World!\n")
+    file.write("For fun, Ive been going to the gym\n")
+
